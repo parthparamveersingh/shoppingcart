20c32d8494adbe799452f9b062b2692f40e5266f
unknown <parthparamveer@gmail.com>
2022-12-05 16:20:27 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,24 @@
+const data = {
+    products: [
+      {
+        id: '1',
+        name: 'samsung phone',
+        price: 1000,
+        image: 'https://i.gadgets360cdn.com/large/samsung_galaxy_s21_ultra_5g_first_impressions_thumb_1611214318279.jpg',
+      },
+      {
+        id: '2',
+        name: 'Toys',
+        price: 90,
+        image: 'https://images.indianexpress.com/2019/09/toys.jpg',
+      },
+      {
+        id: '3',
+        name: 'Cloth',
+        price: 100,
+        image: 'https://api.letsdiskuss.com/resources/static/asset/uploads/1650505823637-aviv-rachmadian-7F7kEHj72MQ-unsplash-scaled.jpg',
+      },
+    ],
+  };
+  export default data;
+  
